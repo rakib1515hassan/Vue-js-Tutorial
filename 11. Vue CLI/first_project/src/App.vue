e1277e8b31308a489125302ebbd477519f94e4fd
Abu Ubaida <rakib1515hassan@gmail.com>
2024-08-27 16:12:45 +0600

    Show Data

--- 11. Vue CLI/first_project/src/App.vue
+++ 11. Vue CLI/first_project/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>User Information</h1>
+    <p>Name: {{ name }}</p>
+    <p>Name: {{ age }}</p>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      name: 'Md Rakib Hassan',
+      age: 27
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
