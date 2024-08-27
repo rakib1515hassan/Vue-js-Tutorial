cc3590b371acc66ed3d6feca24ae22d394c21771
Abu Ubaida <rakib1515hassan@gmail.com>
2024-08-27 16:48:38 +0600

    Update

--- 11. Vue CLI/first_project/src/App.vue
+++ 11. Vue CLI/first_project/src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
+    <app-header></app-header>
+
     <h1>User Information</h1>
     <p>Name: {{ name }}</p>
-    <p>Name: {{ age }}</p>
+    <p>Age: {{ age }}</p>
+
+    <app-footer></app-footer>
   </div>
 </template>
 
-
 <script>
+import AppHeader from "./components/Layout/AppHeader.vue";
+import AppFooter from "./components/Layout/AppFooter.vue";
+
 export default {
-  data(){
+  data() {
     return {
       name: 'Md Rakib Hassan',
       age: 27
-    }
+    };
+  },
+  components: {
+    'app-header': AppHeader,
+    'app-footer': AppFooter
   }
-}
+};
 </script>
 
 <style>
-
+/* Your styles here */
 </style>
